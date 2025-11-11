b7c0835cc7cb351f23e2d3f03c6904ca5903af8d
Peter Mowen <peter.m.mowen@gmail.com>
2025-11-11 10:56:48 -0500

    Add logic to change square color is mouse "hovers" over it
    Chose the 'mouseover' event because it fires when a pointer enters an
    element or any of its children. This allows us to attach a since event
    listener to the container and capture mouseover events for all of the
    squares in the container. Adds an ID to each square the document can be
    queried for the particular square. Then add a class to the square's
    class list to add color to the square.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,6 @@
-let container = document.querySelector('#etch-a-sketch-container');
+const CONTAINER_ID = "etch-a-sketch-container"
+
+let container = document.querySelector(`#${CONTAINER_ID}`);
 let squaresPerSide = 16;
 
 for (let i = 0; i < squaresPerSide; ++i) {
@@ -8,7 +10,18 @@ for (let i = 0; i < squaresPerSide; ++i) {
     let square = document.createElement('div');
     square.classList.add('square');
     square.style.width = `${container.clientWidth / squaresPerSide}px`;
+    square.id = `square-${i}-${j}`;
     column.appendChild(square);
   }
   container.appendChild(column);
 }
+
+container.addEventListener('mouseover', (event) => {
+  let targetID = event.target.id;
+
+  if (targetID != CONTAINER_ID) {
+    console.log(targetID);
+    let square = document.querySelector(`#${targetID}`);
+    square.classList.add("color");
+  }
+});
